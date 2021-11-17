2a1f5ed9b03a51e8c7511f3704ab1a4df19dfb7f
garbagenskij <gretacozza99@gmail.com>
2021-11-17 11:33:43 +0100

    added sketch

--- server.js
+++ server.js
@@ -2,4 +2,14 @@
 // in questo caso scrivo: node server.js, e se funziona compare "up and running" nel terminale
 console.log("up and running");
 // now type npm init and follow instruction, when at the end it ask to type yes do it and then here it will appear a new file: package.json
-// now type npm install express, succedono cose
+// now type npm install express, succedono cose, compaiono nuove righe  in package.json e pure node_modules
+let express = require("express");
+let app = express();
+let port = 3000;
+let server = app.listen(port);
+console.log("server is running on http://localhost:" + port);
+// to check if it works, everytime type node server.js
+app.use(express.static("public"));
+//control + c per bloccare i processi????
+// di nuovo node.js
+// referesha la pagian
