cd454e6e154720718eae2b528ed7755c47019199
garbagenskij <gretacozza99@gmail.com>
2021-11-17 11:08:53 +0100

    initialise project

---
+++ server.js
@@ -0,0 +1,5 @@
+// da github desktop > repository > open in Terminal > scrivi: node (nome file)
+// in questo caso scrivo: node server.js, e se funziona compare "up and running" nel terminale
+console.log("up and running");
+// now type npm init and follow instruction, when at the end it ask to type yes do it and then here it will appear a new file: package.json
+// now type npm install express, succedono cose
